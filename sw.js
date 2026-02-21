03de4cf5a39b0a37882888bdba5b53a4ca503d6a
TomTom630 <tomita.t630@gmail.com>
2026-02-21 16:44:09 +0900

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'neuvates-mikke-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './dashboard.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(res => {
+        if (!res || res.status !== 200 || res.type !== 'basic') return res;
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+    ))
+  );
+});
